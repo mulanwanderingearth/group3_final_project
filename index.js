e578896da7493f3f84474c7fb99019d6ff4ab3d1
Jezmora-duh <jmora18@ivytech.edu>
2024-11-26 14:58:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Course Management System</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Welcome to the College Course Management System</h1>
+      <p>Your one-stop solution to manage courses and schedules!</p>
+    </header>
+
+    <nav>
+      <ul>
+        <li><a href="/teacher">Teacher Portal</a></li>
+        <li><a href="/student">Student Portal</a></li>
+        <li><a href="/course.html">Courses</a></li>
+        <li><a href="/register">Register</a></li>
+      </ul>
+    </nav>
+
+    <main>
+      <section>
+        
+      </section>
+
+      
+    </main>
+
+    <footer>
+      <p>&copy; 2024 Course Management System. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
